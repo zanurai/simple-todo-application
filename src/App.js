7c665c392389409455446eeb761fda710fc0669d
Janaki <zanuthulung@gmail.com>
2023-01-19 16:43:49 +0545

    add file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./index.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+const App = () => {
+    const [userInput, setUserInput] = useState(" ");
+    const [todos, setTodo] = useState([]);
+
+
+
+    const addData = () => {
+        setTodo([...todos, userInput]);
+
+
+    }
+
+
+    function handleDelete(userInput) {
+        const removeItem = todos.filter((todos) => {
+            return todos.userInput !== userInput
+        });
+        setTodo(removeItem);
+
+    }
+
+    const mystyle = {
+        color: "white",
+        fontSize: "40px",
+
+    }
+
+    const myStyle = {
+
+        height: "30px",
+        borderRadius: "10px",
+    }
+    /*const myAdd = {
+
+        height: "30px",
+    }
+    const myRemove = {
+
+        height: "30px",
+    }*/
+
+    return (<div>
+        <h1 style={mystyle}>ToDo Application</h1>
+        <input style={myStyle} onChange={(event) => setUserInput([event.target.value])} type="text" />
+        <button className="btn1" onClick={() => addData()}>Add</button>
+        <button className="btn2" onClick={() => handleDelete()}>Delete</button>
+        <ul>
+            {
+                todos.map((todo, index) => <li key={index}>{todo}</li>)
+            }
+        </ul>
+    </div>)
 }
 
 export default App;
